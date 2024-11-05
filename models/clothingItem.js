1145d8bf91bbda1ddc688351ae50d1da628d38bd
MrWatson0531 <135671943+MrWatson0531@users.noreply.github.com>
2024-11-04 18:40:50 -0700

    first sprint 12 commit

---
+++ models/clothingItem.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+
+  weather: {
+    type: String,
+    required: true,
+  },
+
+  imageUrl: {
+    type: String,
+    required: true,
+    validate: {
+      validator(value) {
+        return validator.isURL(value);
+      },
+      message: "You must enter a valid URL",
+    },
+  },
+
+  owner: {
+    type: String,
+    required: true,
+  },
+
+  likes: {
+
+  },
+
+  createdAt:{}
+});
+
+module.exports = mongoose.model("item", clothingItemSchema);
